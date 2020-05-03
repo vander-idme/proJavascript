a36a712765e8d877255f35927883c130f4f9099a
vander-idme <idmevander@gmail.com>
2020-05-02 19:33:09 -0500

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -15,3 +15,9 @@ const player = new MediaPlayer({
 })
 buttonPlay.onclick = () => player.togglePlay()
 buttonMute.onclick = () => player.toggleMute()
+
+if('serviceWorker' in navigator){
+   navigator.serviceWorker.register('./sw.js').catch(error =>{
+      console.log(error.message)
+   })
+}
