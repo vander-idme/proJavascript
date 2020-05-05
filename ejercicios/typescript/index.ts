3fdbf183e898e19131af2421458bf683a2c33777
vander-idme <idmevander@gmail.com>
2020-05-04 23:29:14 -0500

    Funciones en Typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,65 +1,138 @@
 // TIPOS BASICOS
 
-// Boolean
-let muted: boolean = true
-muted = false
+// // Boolean
+// let muted: boolean = true
+// muted = false
 
-// Numeros
-let numerador: number = 42
-let denomidanor: number = 2
-let resultado = numerador / denomidanor
+// // Numeros
+// let numerador: number = 42
+// let denomidanor: number = 2
+// let resultado = numerador / denomidanor
 
-// String
-let nombre: string = 'Vander'
-let saludo = `Me llamo ${nombre}`
+// // String
+// let nombre: string = 'Vander'
+// let saludo = `Me llamo ${nombre}`
 
-// Arreglo
-let people: string[] = []
-people = ['Isabel', 'Nicole', 'Raul']
+// // Arreglo
+// let people: string[] = []
+// people = ['Isabel', 'Nicole', 'Raul']
 
-  // people.push("2000")
+//   // people.push("2000")
 
-// arreglo que permite dos tipos de variables
-let peopleAndNumbers: Array< string | number > = []
-peopleAndNumbers.push('Vander')
-peopleAndNumbers.push('23')
-console.log(peopleAndNumbers)
+// // arreglo que permite dos tipos de variables
+// let peopleAndNumbers: Array< string | number > = []
+// peopleAndNumbers.push('Vander')
+// peopleAndNumbers.push('23')
+// console.log(peopleAndNumbers)
 
-// Enum
-enum Color {
-  Rojo = "Rojo",
-  Verde = "Verde",
-  Azul = "Azul"
+// // Enum
+// enum Color {
+//   Rojo = "Rojo",
+//   Verde = "Verde",
+//   Azul = "Azul"
+// }
+
+// let colorFavorito: Color = Color.Rojo
+// console.log(`Mi color favorito es ${colorFavorito}`)
+
+// // Any
+// let comodin: any = "Joker"
+// console.log(comodin)
+
+// comodin = { type: 'Wildcard'}
+// console.log(comodin)
+
+// // Object
+// let someObject: object = { type: "Wildcard" }
+// const person = {
+//   name: 'Vander',
+//   edad: 23
+// }
+
+// let persons = []
+// persons.push(person)
+// persons.push("Zhaida")
+
+// console.log(persons)
+
+// // TUPLAS
+// let x: [string, number]
+// x = ["Hello", 10]
+
+// // x = [10, "Hello"]
+
+// console.log(x[0].substring(1))
+// // console.log(x[1].substring(1)) // error, en un numero no hay substring
+
+// FUNCIONES EN TYPE SCRIPT
+
+//podemos ser explicitos en los parametros
+function addNumbers(a: number, b: number) {
+  return a + b
 }
 
-let colorFavorito: Color = Color.Rojo
-console.log(`Mi color favorito es ${colorFavorito}`)
+console.log(addNumbers(1,2))
 
-// Any
-let comodin: any = "Joker"
-console.log(comodin)
-
-comodin = { type: 'Wildcard'}
-console.log(comodin)
-
-// Object
-let someObject: object = { type: "Wildcard" }
-const person = {
-  name: 'Vander',
-  edad: 23
+// y podemos ser aun mas explicitos con el tipo de retorno de la funcion
+function addNumbers2(a: number, b: number): string {
+  return a + '' + b
 }
 
-let persons = []
-persons.push(person)
-persons.push("Zhaida")
+let concat = addNumbers2(2,2)
+console.log(concat)
+console.log(typeof(concat))
 
-console.log(persons)
+function concatenacion(a: string, b: string): string {
+  return a + b
+}
 
-// TUPLAS
-let x: [string, number]
-x = ["Hello", 10]
+let concat2 = concatenacion("2","2")
+console.log(concat2)
+console.log(typeof(concat2))
 
-// x = [10, "Hello"]
+// funciones que retornan funciones
 
-console.log(x[0].substring(1))
-// console.log(x[1].substring(1)) // error, en un numero no hay substring
+function createAdder(a: number): (number) => number{
+  return function(b: number) {
+    return b + a
+  }
+}
+
+const addFour = createAdder(4)
+const fourPlusSix = addFour(6)
+
+// otra forma de declarar funciones que retornan funciones
+let myAdd:(a: number, b: number) => number =
+  function(x: number, y:number): number {return x + y}
+    /*Traduccion de lo de alli arriba:
+      myAdd es una funcion anonima que recibe dos numeros y retorna un numeros
+      let myAdd                                             <- Javascript
+      let myAdd: let myAdd:(a: number, b: number) => number <- Typescript
+
+      y es igual a la funcion anonima que recibe dos numeros y retorna un numero
+      = function(x,y) {return x+y}                          <- Javascript
+      = function(x: number, y:number): number {return x + y}<- Typescript
+    */
+
+console.log('myAdd: ' + myAdd)
+console.log('myAdd(2,3): ' + myAdd(2,3))
+
+
+// funciones con parametros opcionales
+function fullName(firstName: string, lastName?: string): string {
+  return `${firstName} ${lastName}`
+}
+
+const vander = fullName('Vander', 'Idme')
+const zhaida = fullName('Zhaida') //el valor que no le pasamos sera tomado por undefined
+
+//funciones con parametros con valor por omision(por default)
+function fullName2(firstName: string, lastName: string = "Smith"): string {
+  return `${firstName} ${lastName}`
+}
+
+const agente = fullName2("Agente")
+
+console.log(vander)
+console.log(zhaida)
+console.log(agente)
