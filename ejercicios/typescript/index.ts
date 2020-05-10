2b6a01221f13a30c9d2873cfb1fac261b0db3589
vander-idme <idmevander@gmail.com>
2020-05-09 21:08:08 -0500

    Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -67,72 +67,117 @@
 // FUNCIONES EN TYPE SCRIPT
 
 //podemos ser explicitos en los parametros
-function addNumbers(a: number, b: number) {
-  return a + b
+// function addNumbers(a: number, b: number) {
+//   return a + b
+// }
+
+// console.log(addNumbers(1,2))
+
+// // y podemos ser aun mas explicitos con el tipo de retorno de la funcion
+// function addNumbers2(a: number, b: number): string {
+//   return a + '' + b
+// }
+
+// let concat = addNumbers2(2,2)
+// console.log(concat)
+// console.log(typeof(concat))
+
+// function concatenacion(a: string, b: string): string {
+//   return a + b
+// }
+
+// let concat2 = concatenacion("2","2")
+// console.log(concat2)
+// console.log(typeof(concat2))
+
+// // funciones que retornan funciones
+
+// function createAdder(a: number): (number) => number{
+//   return function(b: number) {
+//     return b + a
+//   }
+// }
+
+// const addFour = createAdder(4)
+// const fourPlusSix = addFour(6)
+
+// // otra forma de declarar funciones que retornan funciones
+// let myAdd:(a: number, b: number) => number =
+//   function(x: number, y:number): number {return x + y}
+//     /*Traduccion de lo de alli arriba:
+//       myAdd es una funcion anonima que recibe dos numeros y retorna un numeros
+//       let myAdd                                             <- Javascript
+//       let myAdd: let myAdd:(a: number, b: number) => number <- Typescript
+
+//       y es igual a la funcion anonima que recibe dos numeros y retorna un numero
+//       = function(x,y) {return x+y}                          <- Javascript
+//       = function(x: number, y:number): number {return x + y}<- Typescript
+//     */
+
+// console.log('myAdd: ' + myAdd)
+// console.log('myAdd(2,3): ' + myAdd(2,3))
+
+
+// // funciones con parametros opcionales
+// function fullName(firstName: string, lastName?: string): string {
+//   return `${firstName} ${lastName}`
+// }
+
+// const vander = fullName('Vander', 'Idme')
+// const zhaida = fullName('Zhaida') //el valor que no le pasamos sera tomado por undefined
+
+// //funciones con parametros con valor por omision(por default)
+// function fullName2(firstName: string, lastName: string = "Smith"): string {
+//   return `${firstName} ${lastName}`
+// }
+
+// const agente = fullName2("Agente")
+
+// console.log(vander)
+// console.log(zhaida)
+// console.log(agente)
+
+// INTERFACES
+enum Color {
+  rojo = "Rojo",
+  azul = "Azul"
 }
 
-console.log(addNumbers(1,2))
 
-// y podemos ser aun mas explicitos con el tipo de retorno de la funcion
-function addNumbers2(a: number, b: number): string {
-  return a + '' + b
+interface Rectangulo {
+  ancho: number,
+  alto: number
+  color?: Color //propiedad que puede ser obviada
 }
 
-let concat = addNumbers2(2,2)
-console.log(concat)
-console.log(typeof(concat))
-
-function concatenacion(a: string, b: string): string {
-  return a + b
+const rect: Rectangulo = {
+  ancho: 12,
+  alto: 4,
+  color: Color.azul
 }
 
-let concat2 = concatenacion("2","2")
-console.log(concat2)
-console.log(typeof(concat2))
-
-// funciones que retornan funciones
-
-function createAdder(a: number): (number) => number{
-  return function(b: number) {
-    return b + a
-  }
+function calcularArea(r: Rectangulo): number {
+  return r.alto * r.ancho
 }
 
-const addFour = createAdder(4)
-const fourPlusSix = addFour(6)
+const areaRect = calcularArea(rect)
+console.log(areaRect)
 
-// otra forma de declarar funciones que retornan funciones
-let myAdd:(a: number, b: number) => number =
-  function(x: number, y:number): number {return x + y}
-    /*Traduccion de lo de alli arriba:
-      myAdd es una funcion anonima que recibe dos numeros y retorna un numeros
-      let myAdd                                             <- Javascript
-      let myAdd: let myAdd:(a: number, b: number) => number <- Typescript
-
-      y es igual a la funcion anonima que recibe dos numeros y retorna un numero
-      = function(x,y) {return x+y}                          <- Javascript
-      = function(x: number, y:number): number {return x + y}<- Typescript
-    */
-
-console.log('myAdd: ' + myAdd)
-console.log('myAdd(2,3): ' + myAdd(2,3))
-
-
-// funciones con parametros opcionales
-function fullName(firstName: string, lastName?: string): string {
-  return `${firstName} ${lastName}`
+rect.toString = function() {
+  return this.color?`Un rectangulo color ${this.color}`: `Un rectangulo`
 }
 
-const vander = fullName('Vander', 'Idme')
-const zhaida = fullName('Zhaida') //el valor que no le pasamos sera tomado por undefined
+console.log(rect)
+console.log(rect.toString())
 
-//funciones con parametros con valor por omision(por default)
-function fullName2(firstName: string, lastName: string = "Smith"): string {
-  return `${firstName} ${lastName}`
+interface Cuadrado {
+  lado: number
 }
 
-const agente = fullName2("Agente")
+const cuadro: Cuadrado = {
+  lado: 12
+}
 
-console.log(vander)
-console.log(zhaida)
-console.log(agente)
+console.log(`Este es mi cuadrado: ${cuadro.lado}`)
+
+console.log(`Este es mi cuadrado: ${cuadro.lado}`)
