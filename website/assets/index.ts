d4748109665e366d2adb1aece92b633c641ce7a5
vander-idme <idmevander@gmail.com>
2020-05-11 21:15:59 -0500

    Publicando en NPM

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from './MediaPlayer'
-import AutoPlay from './plugins/AutoPlay'
-import AutoPause from './plugins/AutoPause'
-import Ads from './plugins/Ads'
+import MediaPlayer from '@vanderidme/platzimediaplayer'
+import AutoPlay from '@vanderidme/platzimediaplayer/lib/plugins/AutoPlay'
+import AutoPause from '@vanderidme/platzimediaplayer/lib/plugins/AutoPause'
+import Ads from '@vanderidme/platzimediaplayer/lib/plugins/Ads'
 
 const video = document.querySelector('video')
 const buttonPlay: HTMLElement = document.querySelector('#play')
